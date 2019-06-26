d0a779c92328ddf44be9f75f43f1dd53d6b18c63
Peter Milne <peter.milne@d-tacq.com>
2019-06-26 10:04:30 +0100

    mixed metaphors solved. We have a clean boot .. does it work..  On branch k414 	modified:   acq2106_init_gpio 	modified:   acq2106_init_gpio_common.sh

--- acq2106_init_gpio_common.sh
+++ acq2106_init_gpio_common.sh
@@ -1,38 +1,12 @@
 init_acq2106_leds() {
-# LP3943ISQ
-	
-	LEDSCHIP=$1
-	if [ $? -eq 0 ]; then
-		echo +++ LEDSCHIP FOUND $LEDSCHIP
-
-		cd /sys/class/gpio
-		let LED0=${LEDSCHIP#gpiochip*}
-# LED0 : PWM NOT GPIO 
-		let LED01=$LED0+1
-		let LED1=$LED0+14
-
-	
-		for pin in $(seq $LED01 $LED1)
-		do
-			export_gpio $pin
-			setO gpio${pin}
-		done
-
-		mkln $(gpioLED $LED0 1)  LED/FMC1_G 	AL
-		mkln $(gpioLED $LED0 2)  LED/FMC2_G 	AL
-		mkln $(gpioLED $LED0 3)  LED/FMC3_G	    AL
-		mkln $(gpioLED $LED0 4)  LED/FMC4_G 	AL
-		mkln $(gpioLED $LED0 5)  LED/FMC5_G 	AL
-		mkln $(gpioLED $LED0 6)  LED/FMC6_G 	AL
-		mkln $(gpioLED $LED0 7)  LED/FMC1_R 	AL
-		mkln $(gpioLED $LED0 8)  LED/FMC2_R 	AL
-		mkln $(gpioLED $LED0 9)  LED/FMC3_R 	AL
-		mkln $(gpioLED $LED0 10) LED/FMC4_R 	AL
-		mkln $(gpioLED $LED0 11) LED/FMC5_R 	AL
-		mkln $(gpioLED $LED0 12) LED/FMC6_R 	AL
-		mkln $(gpioLED $LED0 13) LED/ACT_G  	AL
-		mkln $(gpioLED $LED0 14) LED/ACT_R  	AL
-	fi					
+	let px=1
+	for led in FMC1_G FMC2_G FMC3_G FMC4_G FMC5_G FMC6_G \
+			   FMC1_R FMC2_R FMC3_R FMC4_R FMC5_R FMC6_R \
+			   ACT_G  ACT_R
+	do		
+		ln -s /dev/gpio/$1/$(printf P%02d $px)/value /dev/gpio/LED/$led
+		let px=$px+1	
+	done
 }
 
 create_set_fanspeed() {
